479fee895690e790f0bf12c2383636cbb9d8b3d7
adgolden1 <goldalex@bu.edu>
2021-06-04 12:57:24 -0400

    fixed get_prices, added inc_or_dec_prices
    changed the way that getting prices/volumes work. now they just return a full trading day's worth of data back from the time given. I also added "inc_or_dec_prices" which gets the prices, does a linear fit, and returns 1 if the slope is positive and 0 if the slope is negative.

--- data_helper_functions.py
+++ data_helper_functions.py
@@ -7,15 +7,42 @@ Created on Mon May 31 17:05:30 2021
 
 import datetime
 import yfinance as yf
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
-def get_prices_prev_day(ticker_db,final_datetime):
+def get_prices_prev_day(ticker_df,final_datetime,db_dt = 30*60):
     # final datetime should be in datetime objects
+    # db_dt is the sampling frequency of the database in seconds
     final_datetime = final_datetime.replace(microsecond = 0)
-    start_datetime = datetime.datetime.fromtimestamp(final_datetime.timestamp() - 24*60*60)
-    start_datetime = start_datetime.replace(microsecond = 0)
-    # just get the opening prices for that time period
-    return ticker_db['Open'][str(start_datetime):str(final_datetime)]
     
+    # get the index of the last entry before the tweet
+    final_dt_idx = ticker_df.index.get_loc(ticker_df[ticker_df.index <= str(final_datetime)].iloc[-1].name)
+    
+    # get the index of the entry "one day" before the tweet
+    # "one day" in quotes because it ignores time gaps like time between open/close, weekends, etc
+    # a trading day is 6.5 hours long (9:30-4:00)
+    one_day_idxs = int(3600*6.5/db_dt)
+    start_dt_idx = final_dt_idx - one_day_idxs
+    
+    # just get the opening prices for that time period
+    return ticker_df['Open'].iloc[start_dt_idx:final_dt_idx]
+    
+def get_vols_prev_day(ticker_df,final_datetime,db_dt = 30*60):
+    # final datetime should be in datetime objects
+    # db_dt is the sampling frequency of the database in seconds
+    final_datetime = final_datetime.replace(microsecond = 0)
+    
+    # get the index of the last entry before the tweet
+    final_dt_idx = ticker_df.index.get_loc(ticker_df[ticker_df.index <= str(final_datetime)].iloc[-1].name)
+    
+    # get the index of the entry "one day" before the tweet
+    # "one day" in quotes because it ignores time gaps like time between open/close, weekends, etc
+    # a trading day is 6.5 hours long (9:30-4:00)
+    one_day_idxs = int(3600*6.5/db_dt)
+    start_dt_idx = final_dt_idx - one_day_idxs
+    
+    # just get the opening prices for that time period
+    return ticker_df['Volume'].iloc[start_dt_idx:final_dt_idx]
     
     
 def generate_ticker_db(symbol,first_dt,last_dt):
@@ -27,3 +54,13 @@ def generate_ticker_db(symbol,first_dt,last_dt):
     tick = yf.Ticker(symbol)
     ticker_db = tick.history(interval = '30m',start = first_dt,end = last_dt)
     return ticker_db
+
+def inc_or_dec_prices(ticker_db,ticker,dt):
+    # function for returning 0 or 1 based on whether the price data for a specific ticker increases or decreases
+    # datetime should be a datetime object corresponding to a tweet
+    prices = get_prices_prev_day(ticker_db,datetime).to_numpy()
+    idxs = np.arange(prices.size)
+    reg = LinearRegression().fit(idxs.reshape(-1,1), prices.reshape(-1,1))
+    slope = reg.coef_[0,0]
+    return np.heaviside(slope,0.5)
+    
